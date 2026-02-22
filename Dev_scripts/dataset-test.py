28a80c2631aeac8b2dede6a5c033c11123546f95
Farzan R S <farzanrichu610@gmail.com>
2026-02-22 21:53:53 +0530

    dataset verification done

--- Dev_scripts/dataset-test.py
+++ Dev_scripts/dataset-test.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 #for testing datasets
-voltage_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Voltage\\voltage_0.xlsx"
-current_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Current\\current_0.xlsx"
+voltage_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Voltage\\voltage_0.csv"
+current_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Current\\current_0.csv"
 
 
 for dataset in [voltage_dataset,current_dataset]:
-    df = pd.read_excel(dataset, header=1)
+    df = pd.read_csv(dataset, header=1)
     print(df.head(20))
