aec53d65132b780c0bdf344f96b556f769bf3a32
Farzan R S <farzanrichu610@gmail.com>
2026-03-06 02:23:04 +0530

    colum names verifications

--- Dev_scripts/dataset-test.py
+++ Dev_scripts/dataset-test.py
@@ -6,5 +6,22 @@ current_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Current\\cu
 
 
 for dataset in [voltage_dataset,current_dataset]:
-    df = pd.read_csv(dataset, header=1)
-    print(df.head(20))
+    df = pd.read_csv(dataset)
+
+
+    print(df.columns)
+
+    # The columns are
+    #     STARTDATE
+    #     SubstationId
+    #     DeviceType
+    #     DeviceId
+    #     PointName
+    #     MeasurementId
+    #     MeasurementData25hz
+    #     MeasurementData25hz: Quality
+
+# we only need
+# STARTDATE
+# PointName
+# MeasurementData25hz
