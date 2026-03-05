d351de5e245758c68ede5ed7bcbbb871045359b7
Farzan R S <farzanrichu610@gmail.com>
2026-03-06 02:39:30 +0530

    verified data cleaning

--- Dev_scripts/dataset-test.py
+++ Dev_scripts/dataset-test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 #for testing datasets
-voltage_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Voltage\\voltage_0.csv"
-current_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Current\\current_0.csv"
+voltage_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Voltage\\cleaned\\voltage_0.csv"
+current_dataset="C:\\Users\\farza\\transmission-faults-ml\\Datasets\\Current\\cleaned\\current_0.csv"
 
 
 for dataset in [voltage_dataset,current_dataset]:
